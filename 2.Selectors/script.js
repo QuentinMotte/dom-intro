9670f8583d830444f9cecbfce7e43270aea25de8
Quentin Motte <motte.quentin1@gmail.com>
2022-06-01 15:53:33 +0200

    add correction

--- 2.Selectors/script.js
+++ 2.Selectors/script.js
@@ -1,22 +1,16 @@
 //
 const importantElement = document.querySelectorAll(".important");
-importantElement.forEach(function (item) {
-    item.title = "this is an important item";
+importantElement.forEach(function (p) {
+    p.title = "this is an important item";
 });
 //
 //
 const image = document.querySelectorAll("img");
-image.forEach(function (item) {
-    for (let i = 0; i < item.length; i++) {
-        if ((item.classList = "important")) {
-            item.style.display = "inline";
-        } else {
-            item.style.display = "none";
-        }
+image.forEach((item) => {
+    if (item.classList != "important") {
+        item.style.display = "none";
     }
-    console.log(image);
 });
-//
 //
 const paragraphs = document.querySelectorAll("p");
 paragraphs.forEach(function (item) {
@@ -26,12 +20,12 @@ paragraphs.forEach(function (item) {
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
     const colorRandom = `rgb(${r},${g},${b})`;
-    item.style.color = colorRandom;
+    if (item.classList != "important") item.style.color = colorRandom;
 });
 //
 //
-const paragraphsImportant = document.querySelectorAll("p.important");
-paragraphsImportant.forEach((p) => {
-    p.style.color = "black";
-});
-console.log(paragraphsImportant);
+// const paragraphsImportant = document.querySelectorAll("p.important");
+// paragraphsImportant.forEach((p) => {
+//     p.style.color = "black";
+// });
+// console.log(paragraphsImportant);
